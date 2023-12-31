e8fa601bd91012c05dacf3294956bdf25eb53f17
Camilo Gaon <camilogaon13@gmail.com>
2023-12-31 17:44:28 -0500

    version2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -123,8 +123,8 @@ export default function HomePage() {
 
             {/* Skills */}
             <section className='grid h-[400px] bg-white'>
-                <div style={{alignItems: 'center', marginBottom:''}}>
-                    <p style={{fontSize:'40px', fontWeight:'600', marginTop:'15px', textAlign: 'center'}}>SKILLS</p>
+                <div style={{alignItems: 'center', marginBottom:'50px'}}>
+                    <p className='text-green-400' style={{fontSize:'40px', fontWeight:'600', marginTop:'15px', textAlign: 'center'}}>SKILLS</p>
                 </div>
                 <div className='flex justify-center space-x-20'>
                     <div className='text-center'>
@@ -216,14 +216,40 @@ export default function HomePage() {
             </section>
 
             {/* Proyectos */}
-            <section className='grid h-[786px] bg-green-400'>
-            <div style={{alignItems: 'center', marginBottom:''}}>
-                    <p style={{fontSize:'40px', fontWeight:'600', marginTop:'15px', textAlign: 'center'}}>CV</p>
+            <section className='grid h-[500px] bg-white' >
+                <div style={{alignItems: 'center', marginTop: '30px'}}>
+                    <p className='text-green-400' style={{fontSize:'40px', fontWeight:'600', marginTop:'15px', textAlign: 'center'}}>CV</p>
                 </div>
+                <div className='' style={{width: '1080px',  marginLeft: 'auto', marginRight: 'auto' }}>
+                    <p className='text-justify'>Soy Anderson Camilo Gaon, un estudiante de décimo semestre de ingeniería de sistemas en la Universidad de Nariño. Además, cuento con un técnico en diseño e integración en multimedia, obtenida en el Servicio Nacional de Aprendizaje (SENA), así como en sistemas, gracias a mi título de técnico en sistemas del Instituto Técnico Sistematizado de Nariño.<br/><br/>
+                    Mi pasión por la programación y el desarrollo de software me ha llevado a adquirir habilidades en diversos lenguajes de programación, tecnologías y bases de datos. Entre mis competencias se incluyen HTML, CSS, JavaScript, Java, PHP, Python, React, Angular, Laravel, Flask, PostgreSQL, MySQL, así como el manejo experto de herramientas como Git y GitHub. Esta amplia gama de conocimientos me permite abordar proyectos de manera integral, desde la conceptualización hasta la implementación y el mantenimiento.<br/><br/>
+                    Además de mi habilidad técnica, me destaco por mi capacidad para trabajar en equipo. Soy receptivo al cambio y a la retroalimentación, lo que me permite adaptarme eficientemente a nuevos desafíos y aprender de experiencias anteriores. Mi enfoque colaborativo se traduce en la creación de soluciones efectivas y eficientes.
+                    </p>
+                    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                        <a href="https://drive.google.com/file/d/1Kx2n3RHf9n-d6jUk19F8CWrcLpzm6NYR/view?usp=sharing" target="_blank" className="bg-green-500 px-4 py-2 rounded-full text-white inline-flex items-center">
+                            <span className="mr-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" fill='white'/></svg>
+                            </span>
+                            Ver CV
+                        </a>
+                    </div>
+                </div>
+                
             </section>
 
             <footer className="bg-gray-800 text-white p-4 text-center">
-                <p>&copy; 2023 Camilo Gaon. Todos los derechos reservados.</p>
+                <div className='flex justify-center space-x-7'>
+                    <p>&copy; 2024 Camilo Gaon. Todos los derechos reservados.</p>
+                    <a href='https://github.com/camilogaon' target="_blank" rel="noopener noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" fill='white'/></svg>
+                        </a>
+                        <a href='https://www.linkedin.com/in/anderson-camilo-gaon-melo-827241246?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' target="_blank" rel="noopener noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 39 39" fill="none">
+                                <path d="M19.2 0C8.596 0 0 8.59604 0 19.2001C0 29.8041 8.596 38.4002 19.2 38.4002C29.804 38.4002 38.4 29.8041 38.4 19.2001C38.4 8.59604 29.804 0 19.2 0ZM14.5 27.1581H10.612V14.6461H14.5V27.1581ZM12.532 13.1101C11.304 13.1101 10.51 12.2401 10.51 11.1641C10.51 10.066 11.328 9.22204 12.582 9.22204C13.836 9.22204 14.604 10.066 14.628 11.1641C14.628 12.2401 13.836 13.1101 12.532 13.1101ZM28.7 27.1581H24.812V20.2241C24.812 18.6101 24.248 17.5141 22.842 17.5141C21.768 17.5141 21.13 18.2561 20.848 18.9701C20.744 19.2241 20.718 19.5841 20.718 19.9421V27.1561H16.828V18.6361C16.828 17.0741 16.778 15.7681 16.726 14.6441H20.104L20.282 16.3821H20.36C20.872 15.5661 22.126 14.3621 24.224 14.3621C26.782 14.3621 28.7 16.0761 28.7 19.7601V27.1581Z" fill="white"/>
+                            </svg>
+                        </a>
+                </div>
+                
             </footer>
 
         </main>
