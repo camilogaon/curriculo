db787827ebc5db2a38071242bf012443c3b302a8
Camilo Gaon <camilogaon13@gmail.com>
2023-12-31 15:04:11 -0500

    version1

---
+++ src/ui/font.ts
@@ -0,0 +1,5 @@
+import { Plus_Jakarta_Sans } from "next/font/google"
+
+
+
+export const plus_Jakarta_Sans = Plus_Jakarta_Sans({subsets:["latin"], weight:["200","300","400","500","600", "700", "800"]})
